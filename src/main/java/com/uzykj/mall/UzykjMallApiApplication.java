c862e621a0cd78e752ff7763c6b0eedb5845e4c4
ghostxbh <ghostxbh@hotmail.com>
2019-05-19 18:52:05 +0800

    启动mapper扫描

--- src/main/java/com/uzykj/mall/UzykjMallApiApplication.java
+++ src/main/java/com/uzykj/mall/UzykjMallApiApplication.java
@@ -1,9 +1,11 @@
 package com.uzykj.mall;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(value = "com.uzykj.mall.dao")
 public class UzykjMallApiApplication {
 
     public static void main(String[] args) {

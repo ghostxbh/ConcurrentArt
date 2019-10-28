00330e0773477bb8d58432d63717a0217d8a6844
ghostxbh <ghostxbh@hotmail.com>
2019-10-28 15:38:55 +0800

    新增并发专题

---
+++ src/problem/visible/VisibleSyn.java
@@ -0,0 +1,41 @@
+package problem.visible;
+
+/**
+ * 使用synchronized解决可见性
+ * Created by xbh 2019-10-22
+ */
+public class VisibleSyn {
+    private static byte[] obj = new byte[0];
+    private static boolean stop;
+
+    /**
+     * 输出：
+     * A is running
+     * B is running
+     * B is end
+     * A is end
+     */
+    public static void main(String[] args) throws InterruptedException {
+        new Thread(() -> {
+            System.out.println("A is running");
+            synchronized (obj){
+                try {
+                    obj.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                while (!stop);
+            }
+            System.out.println("A is end");
+        }).start();
+        Thread.sleep(10);
+        new Thread(() -> {
+            System.out.println("B is running");
+            synchronized (obj){
+                stop = true;
+                obj.notify();
+            }
+            System.out.println("B is end");
+        }).start();
+    }
+}
---
+++ src/problem/visible/VisibleTest.java
@@ -0,0 +1,40 @@
+package problem.visible;
+
+/**
+ * 可见性问题
+ * Created by xbh 2019-10-18
+ */
+public class VisibleTest {
+    private static long count = 0;
+
+    void add10K() {
+        int idx = 0;
+        while (idx++ < 10000) {
+            count += 1;
+        }
+    }
+
+    public static long acdc() throws InterruptedException {
+        final VisibleTest visibleTest = new VisibleTest();
+        Thread thread1 = new Thread(() -> visibleTest.add10K());
+        Thread thread2 = new Thread(() -> visibleTest.add10K());
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+        return count;
+    }
+
+    /**
+     * 输出： 10000 < count < 20000
+     */
+    public static void main(String[] args) {
+        try {
+            System.out.println(VisibleTest.acdc());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
